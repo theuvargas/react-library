b9bb04759a9b4cfbeb2a8f1dd870b617f3be419d
Matheus Vargas <matheusvargas412@gmail.com>
2021-10-29 18:29:07 -0300

    switched to hashrouter

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import { ChakraProvider, Box } from '@chakra-ui/react';
+import { ChakraProvider } from '@chakra-ui/react';
 import theme from './theme';
-import { Switch, BrowserRouter as Router, Route } from 'react-router-dom';
+import { Switch, HashRouter as Router, Route } from 'react-router-dom';
 import BooksDisplay from './components/books-display/BooksDisplay';
 import BookPage from './components/book-page/BookPage';
 import Navbar from './components/ui/nav/Navbar';
@@ -12,10 +12,10 @@ function App() {
       <Router>
         <Navbar />
         <Switch>
-          <Route path="https://theuvargas.github.io/react-library/books/:bookId">
+          <Route path="/books/:bookId">
             <BookPage />
           </Route>
-          <Route path="https://theuvargas.github.io/react-library/" exact>
+          <Route path="/" exact>
             <BooksDisplay />
           </Route>
           <Route path="*">error 404</Route>
