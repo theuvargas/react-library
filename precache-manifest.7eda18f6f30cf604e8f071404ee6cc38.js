bc29d4f0396f59cd78715eb55a837d045a710b47
Matheus Vargas <matheusvargas412@gmail.com>
2021-10-29 18:06:18 -0300

    Updates

---
+++ precache-manifest.7eda18f6f30cf604e8f071404ee6cc38.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ec37d7acc0f952cf0cce37ed8e4f039",
+    "url": "/react-library/index.html"
+  },
+  {
+    "revision": "6868ff02abd880dadc69",
+    "url": "/react-library/static/js/2.5ba11315.chunk.js"
+  },
+  {
+    "revision": "d18dba208164d01c98054a17fb168340",
+    "url": "/react-library/static/js/2.5ba11315.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "88bc243e99e579bd3d77",
+    "url": "/react-library/static/js/3.1967adfb.chunk.js"
+  },
+  {
+    "revision": "e9b3fa9b99dd72c11b40",
+    "url": "/react-library/static/js/main.65d9108f.chunk.js"
+  },
+  {
+    "revision": "803bede2ada609f1cb99",
+    "url": "/react-library/static/js/runtime-main.d0255c3d.js"
+  }
+]);
